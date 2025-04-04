46b9cec05c395ca0fc3ce4a677e87ac98eed64ff
Ashley Buchanan <ashleyshanebuchanan@gmail.com>
2025-04-04 16:19:33 -0700

    finished both insertion methods

--- binary-search-tree.js
+++ binary-search-tree.js
@@ -15,14 +15,60 @@ class BinarySearchTree {
      * Returns the tree. Uses iteration. */
 
     insert(val) {
+        let currentNode = this.root;
+        if (currentNode === null) {
+            this.root = new Node(val);
+            return this;
+        }
+        while (currentNode) {
+            if (val === currentNode.val) return this;
 
+            if (val < currentNode.val) {
+                if (currentNode.left === null) {
+                    currentNode.left = new Node(val);
+                }
+                currentNode = currentNode.left;
+            } else {
+                if (currentNode.right === null) {
+                    currentNode.right = new Node(val);
+                }
+                currentNode = currentNode.right;
+            }
+        }
+        return this;
     }
 
     /** insertRecursively(val): insert a new node into the BST with value val.
      * Returns the tree. Uses recursion. */
 
     insertRecursively(val) {
+        let currentNode = this.root;
+        if (currentNode === null) {
+            this.root = new Node(val);
+            return this;
+        }
 
+        function helper(n) {
+            if (val < n.val) {
+
+                if (n.left === null) {
+                    n.left = new Node(val);
+                } else {
+                    helper(n.left)
+                }
+
+            } else if (val > n.val) {
+
+                if (n.right === null) {
+                    n.right = new Node(val);
+                } else {
+                    helper(n.right);
+                }
+            }
+        }
+
+        helper(currentNode)
+        return this;
     }
 
     /** find(val): search the tree for a node with value val.
@@ -90,5 +136,16 @@ class BinarySearchTree {
 
     }
 }
+
+const binarySearchTree = new BinarySearchTree();
+binarySearchTree.insertRecursively(15);
+binarySearchTree.insertRecursively(20);
+binarySearchTree.insertRecursively(10);
+binarySearchTree.insertRecursively(12);
+
+console.log(binarySearchTree.root.val);
+console.log(binarySearchTree.root.right.val);
+console.log(binarySearchTree.root.left.val);
+console.log(binarySearchTree.root.left.right.val);
 
 module.exports = BinarySearchTree;
