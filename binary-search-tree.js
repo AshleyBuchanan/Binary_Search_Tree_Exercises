7918229a567ea70863f44e22d580b3c114d1f974
Ashley Buchanan <ashleyshanebuchanan@gmail.com>
2025-04-05 10:50:54 -0700

    finished dfs_orders

--- binary-search-tree.js
+++ binary-search-tree.js
@@ -75,35 +75,84 @@ class BinarySearchTree {
      * return the node, if found; else undefined. Uses iteration. */
 
     find(val) {
+        let currentNode = this.root;
+        if (currentNode === null) return 'undefined';
 
+        while (currentNode) {
+            console.log(`val:${val} <-> currentNode.val:${currentNode.val}`)
+            if (currentNode.val === val) return currentNode;
+            currentNode = val < currentNode.val
+                ? currentNode.left
+                : currentNode.right
+        }
     }
 
     /** findRecursively(val): search the tree for a node with value val.
      * return the node, if found; else undefined. Uses recursion. */
 
     findRecursively(val) {
+        let currentNode = this.root;
+        if (currentNode === null) return 'undefined';
 
+        function helper(n) {
+            if (!n) return null
+            if (val === n.val) return n
+
+            if (val < n.val) {
+                currentNode = helper(n.left)
+            } else {
+                currentNode = helper(n.right)
+            }
+        }
+        helper(currentNode);
+        return currentNode;
     }
+
 
     /** dfsPreOrder(): Traverse the array using pre-order DFS.
      * Return an array of visited nodes. */
-
     dfsPreOrder() {
-
+        const path = [];
+        function helper(node = this.root) {
+            //pre-order
+            path.push(node.val);
+            if (node.left) helper(node.left);
+            if (node.right) helper(node.right);
+        }
+        helper(this.root)
+        return path;
     }
+
+
 
     /** dfsInOrder(): Traverse the array using in-order DFS.
      * Return an array of visited nodes. */
 
     dfsInOrder() {
-
+        const path = [];
+        function helper(node = this.root) {
+            //in-order
+            if (node.left) helper(node.left);
+            path.push(node.val);
+            if (node.right) helper(node.right);
+        }
+        helper(this.root)
+        return path;
     }
 
     /** dfsPostOrder(): Traverse the array using post-order DFS.
      * Return an array of visited nodes. */
 
     dfsPostOrder() {
-
+        const path = [];
+        function helper(node = this.root) {
+            //post-order
+            if (node.left) helper(node.left);
+            if (node.right) helper(node.right);
+            path.push(node.val);
+        }
+        helper(this.root)
+        return path;
     }
 
     /** bfs(): Traverse the array using BFS.
@@ -142,10 +191,22 @@ binarySearchTree.insertRecursively(15);
 binarySearchTree.insertRecursively(20);
 binarySearchTree.insertRecursively(10);
 binarySearchTree.insertRecursively(12);
+binarySearchTree.insertRecursively(1);
+binarySearchTree.insertRecursively(5);
+binarySearchTree.insertRecursively(50);
+
 
 console.log(binarySearchTree.root.val);
 console.log(binarySearchTree.root.right.val);
 console.log(binarySearchTree.root.left.val);
 console.log(binarySearchTree.root.left.right.val);
+
+
+console.log(binarySearchTree.findRecursively(99));
+
+console.log(binarySearchTree.dfsInOrder());
+console.log(binarySearchTree.dfsPostOrder());
+console.log(binarySearchTree.dfsPreOrder());
+
 
 module.exports = BinarySearchTree;
